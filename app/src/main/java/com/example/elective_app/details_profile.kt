1062d0e0e05ba98f25e43a3b6cdb7ef54051ea1a
cyberbaaz <ksh09itij@gmail.com>
2022-06-12 05:31:20 +0530

    User Dashboard Intent

---
+++ app/src/main/java/com/example/elective_app/details_profile.kt
@@ -0,0 +1,11 @@
+package com.example.elective_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class details_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_profile)
+    }
+}
